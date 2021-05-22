f9da98a476177803cf937e858768dbf843a19a46
Qi Zhao <zhaoqistevens@outlook.com>
2021-05-22 11:14:28 -0400

    update HW04a unittest

--- GitHubApi567.py
+++ GitHubApi567.py
@@ -1,4 +1,24 @@
+import requests
+import json
+
+
 def read_from_git_api(user_id):
-    ans = [1,2,3]
-    ans.append(user_id)
+    """
+    this function get the github user_id and query all the user repo
+    :param user_id: the user github id
+    :return: [repo name]
+    """
+    ans = []
+    r = requests.get(f'https://api.github.com/users/{user_id}/repos')
+    if r.status_code!=200 and r.status_code!=403:
+        raise Exception("invalid user id")   #raise ValueError if the user_id does not exsist
+    if r.status_code == 403:
+        raise Exception("too much request, please wait for a hour")
+    response = json.loads(r.text)
+
+    for repo in response:
+        repo_name = repo["name"]
+        r2 = requests.get(f'https://api.github.com/repos/{user_id}/{repo_name}/commits')
+        response2 = json.loads(r2.text)
+        ans.append(f'Repo: {repo["name"]} Number of commits: {len(response2)}')
     return ans
--- test.py
+++ test.py
@@ -6,8 +6,25 @@ import unittest
 class TestGitHubApi567(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testHelloWorld(self): 
-      self.assertEqual(read_from_git_api("qiblaqi"),[1,2,3,"qiblaqi"])
+    def testIfUserIDisValid(self):
+      self.assertEqual(read_from_git_api("qiblaqi"),['Repo: 2020Fall-810-B Number of commits: 21',
+                                                     'Repo: 567-Fall-2020-master Number of commits: 6',
+                                                     'Repo: 567-HW04a Number of commits: 2',
+                                                     'Repo: 567-Spring-2020-qzhao Number of commits: 12',
+                                                     'Repo: 567HW02a Number of commits: 6',
+                                                     'Repo: action-build Number of commits: 30',
+                                                     'Repo: Fall2020-567-533-590 Number of commits: 22',
+                                                     'Repo: Fall2020_567_HW02 Number of commits: 8',
+                                                     'Repo: GitHubApi567 Number of commits: 14',
+                                                     'Repo: InterNatter Number of commits: 27',
+                                                     'Repo: n_r Number of commits: 1',
+                                                     'Repo: Stevens-Python-Course-ByQ.Zhao Number of commits: 44',
+                                                     'Repo: Student-Repository Number of commits: 5'])
+    def testIfInvalidUserID(self):
+        with self.assertRaises(Exception) as context:
+            read_from_git_api("qiblaqi213891038193")    #test invalid user id
+
+        self.assertTrue('invalid user id' in str(context.exception))
    
 
 if __name__ == '__main__':
