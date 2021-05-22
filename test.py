937138f223d0527f8d9f933204592df8b84101de
Qi Zhao <qiblaqi@126.com>
2021-05-22 09:11:46 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+class TestGitHubApi(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testHelloWorld(self): 
+      self.assertEqual(1,1);
+   
+
+if __name__ == '__main__':
+    print('Running unit tests for GitHubApi567!')
+    unittest.main()
