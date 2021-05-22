45d78188e941d4350a9231a993dedac1da3bfd5d
Qi Zhao <zhaoqistevens@outlook.com>
2021-05-22 09:34:45 -0400

    create read_from_git_api function

--- GitHubApi567.py
+++ GitHubApi567.py
@@ -1,2 +1,2 @@
-print("hello!")
-print("update from pycharm")
+def read_from_git_api(user_id):
+    return [1,2,3].append(user_id)
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class TestGitHubApi567(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testHelloWorld(self): 
-      self.assertEqual(1,1);
+      self.assertEqual(read_from_git_api("qiblaqi"),[1,2,3,"qiblaqi"])
    
 
 if __name__ == '__main__':
